5130d0738977688c75a099d16b9bea0fa9788282
Honey Bhasvar <52161860+honey-bhavsar@users.noreply.github.com>
2022-02-06 17:35:23 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import pywhatkit
+import emojis
+
+msg = emojis.encode(":wink: :smile:")
+# pywhatkit.sendwhatmsg('+919998709339', 'Minmin is soooo cuteeee \U0001f600 \U0001F602 \U0001F609 \U0001F608', 10, 00)
+# pywhatkit.sendwhatmsg('+919998709339', 'Emojis aagye.. yaay!! \U0001F483', 10, 1)
+# pywhatkit.sendwhatmsg('+917490059986', 'Minmin is soooo cuteeee \U0001f600 \U0001F602 \U0001F609 \U0001F608', 10, 00)
+
+print("Minmin is soooo cuteeee \U0001f600 \U0001F602 \U0001F609 \U0001F608")
